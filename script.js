854e939d098fc58f58a438f8fc7841f0b80bbc22
Tarsilacloset <tarsilaminishopping@gmail.com>
2026-01-24 11:59:37 -0300

    up

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ const cartCounter = document.getElementById("cart-count") //quantidade do carrin
 const addressInput = document.getElementById("address") // endereço
 const addressWarn = document.getElementById("address-warn")
 
+let cart =[]
+
 
 // abrir o carrinho
 cartBtn.addEventListener("click",function(){
+    updateCartModal()
     cartModal.style.display = "flex"
 })
 // fechar o carrinho se clicar fora
@@ -25,3 +28,67 @@ closeModalBtn.addEventListener("click", function(){
     cartModal.style.display = "none"
 })
 
+
+menu.addEventListener("click",function(event){
+    
+
+    let parentButton = event.target.closest(".add-to-cart-btn")
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = Number(parentButton.getAttribute("data-price"))
+        
+        //Adicionar no carrinho
+        addTocart(name,price)
+    }
+    
+})
+
+// funcao para adicionar no carrinho
+function addTocart(name,price){
+    const existingItem = cart.find(item => item.name === name)
+    if(existingItem){
+        // se o item ja existe, aumenta a quantidade
+        existingItem.quantity += 1
+    }
+    else{
+        cart.push({
+        name,
+        price,
+        quantity: 1,
+    })
+
+    }
+    updateCartModal()
+    
+    
+}
+
+
+// atualizar carrinho
+function updateCartModal(){
+cartItemsContainer.innerHTML = ""
+let total = 0
+
+cart.forEach(item =>{
+    const cartItemElement = document.createElement("div")
+    cartItemElement.classList.add("flex","justify-between","mb-4", "flex-col")
+    cartItemElement.innerHTML =`
+    <div class="flex items-center justify-between ">
+        <div>
+            <p class="font-medium">${item.name} </p>
+            <p>Qtn: ${item.quantity}</p>
+            <p class=" font-medium mt-2">R$ ${item.price.toFixed(2)}</p>        
+        </div>
+        <button>
+             Remover 
+        </button>
+    </div>
+    
+    `
+
+    total += item.price * item.quantity
+
+    cartItemsContainer.appendChild(cartItemElement)
+})
+cartTotal.textContent = total
+}
